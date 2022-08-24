7a88fbfd7f36a90324f69131ba59f25c63118ca1
Your Name <you@example.com>
2022-08-24 14:34:40 +0530

    commit24/8/22

--- app/urls.py
+++ app/urls.py
@@ -8,9 +8,26 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns =[  
 
-    path('admin_dashboard',views.admin_dashboard,name='admin_dashboard'),
+    path('load_login',views.load_login,name='load_login'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+
+
+
+    #======Admin===============
+
+
+    path('load_dashbord',views.load_dashbord,name='load_dashbord'),
+
+
+
+
+    #=============User===============
+
     
     path('',views.load_index,name='load_index'),
+    path('training_load',views.training_load,name='training_load'),
+    path('mail_send',views.mail_send,name='mail_send'),
     path('enquery_send',views.enquery_send,name='enquery_send'),
    
     
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User,auth
 from .models import *
 
 
@@ -15,6 +17,10 @@ def admin_dashboard(request):
 def load_index(request):
     return render(request,'User/index.html')
 
+def training_load(request):
+    return render(request,'User/Training.html')
+
+
 def enquery_send(request):
     
    if request.method=="POST":
@@ -27,4 +33,68 @@ def enquery_send(request):
                     contact=phno,
                     place=locality)
         enq.save()
-        return render(request,'home.html')
+        message="Successfully Registered ."
+        return render(request,'User/Training.html',{'message':message})
+
+
+
+
+
+
+#===============Admin side====================
+
+
+def load_login(request):
+    return render(request,'Admin/login.html')
+
+
+
+def login(request): 
+    try:
+        if request.method == 'POST':
+            try:
+                username = request.POST['username']
+                password = request.POST['password']
+                user = auth.authenticate(username=username, password=password)
+                request.session["uid"] = user.id
+
+                if user is not None:
+                    auth.login(request, user)
+                    if user.is_superuser==1:
+                        
+                        enquerys=Enquery.objects.all()
+                        message=None
+                        return render(request,'Admin/index.html',{'enquerys':enquerys})
+                        
+                    else:
+                        message='Invalid username or password'
+                        return render(request,'Admin/login.html',{'message':message})
+            except:
+                message='Invalid username or password'
+                return render(request,'Admin/login.html',{'message':message})                       
+        else:
+            #messages.info(request, 'Invalid username or password')
+            message='Invalid username or password'
+            return render(request,'Admin/login.html',{'message':message})
+                   
+    except:
+       # messages.info(request, 'Invalid username or password')
+        message='Invalid username or password'
+        return render(request,'Admin/login.html',{'message':message})
+
+
+@login_required(login_url="/load_login")
+def load_dashbord(request):
+    enquerys=Enquery.objects.all()
+    return render(request,'Admin/index.html',{'enquerys':enquerys})
+
+
+def mail_send(request):
+    msg="Mail sended."
+    return render(request,'User/index.html',{'msg':msg})
+
+
+def logout(request):
+    request.session["uid"] = ""
+    auth.logout(request)
+    return redirect('load_index')
